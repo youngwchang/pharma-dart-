762147468b8c88a3a8c0b5bf297d66210d53131a
youngwchang <youngwook.chang@gmail.com>
2026-05-15 07:52:08 +0900

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
+ReactDOM.createRoot(document.getElementById('root')).render(<app/>);
